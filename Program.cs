987fdbbd41669920df1d77ff1e50e05a48101693
Lucas Ladan <l.ladan419@mybvc.ca>
2024-09-19 17:30:35 -0600

    TicTacToeDone

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-bool badInput = true;
+using System.Net;
+using static System.Net.Mime.MediaTypeNames;
 
-LearningActivity31();
-LearningActivity32();
+//LearningActivity31();
+//LearningActivity32();
 LearningActivity33();
-LearningActivity34();
-LearningActivity35();
+//LearningActivity34();
+//LearningActivity35();
 
 int interagerInput(string prompt)//This is to make sure that the entered number is a valid interager
 {
@@ -28,15 +29,16 @@ void LearningActivity31()
     {
         whatUnit = interagerInput("What unit are you using?\n1 for celsius\n2 for fahrenheit");
     } while (whatUnit != 1 && whatUnit != 2);
-    if (whatUnit == 1)
+    switch(whatUnit)
     {
-        temperature = (temperature * (9F / 5F)) + 32F;
-        Console.WriteLine("The temperature in fahrenheit is " + temperature);
-    }
-    else
-    {
-        temperature = (temperature - 32F) * (5F / 9F);
-        Console.WriteLine("The temperature in celsius is " + temperature);
+        case 1:
+            temperature = (temperature * (9F / 5F)) + 32F;
+            Console.WriteLine("The temperature in fahrenheit is " + temperature);
+            break;
+        case 2:
+            temperature = (temperature - 32F) * (5F / 9F);
+            Console.WriteLine("The temperature in celsius is " + temperature);
+            break;
     }
 }
 void LearningActivity32()
@@ -52,26 +54,99 @@ void LearningActivity32()
 void LearningActivity33()
 {
     Random rand = new Random();
-    int[,] board = new int[3,3];
-    for (int i = 0;i < 3;i++)
+    int[,] board = new int[3, 3];
+    int[] weights = { 1, 4, 4 };
+    for (int i = 0; i < 3; i++)
     {
         Console.WriteLine();
-        for (int j = 0; j < 3;j++)
+        for (int j = 0; j < 3; j++)
         {
-            board[i,j] = rand.Next(0, 2);
-            if (board[i,j] == 1)
+            int input;
+            do
+            {
+                input = rand.Next(0, 3);
+            } while (weights[input] == 0);
+            board[i,j] = input;
+            weights[input]--;
+            if (board[i, j] == 1)
             {
                 Console.Write("X  ");
             }
-            else if (board[i,j] == 2)
+            else if (board[i, j] == 2)
             {
-                Console.Write("Y  ");
+                Console.Write("O  ");
             }
             else
             {
                 Console.Write("_  ");
             }
         }
+    }
+    int winner = 0;
+    int heldInt;
+    if (board[0, 0] == 1 || board[0,0] == 2)
+    {
+        heldInt = board[0,0];
+        if (heldInt == board[0,1] && heldInt == board[0, 2])
+        {
+            winner = heldInt;
+        }
+        else if (heldInt == board[1,0] && heldInt == board[2,0])
+        {
+            winner = heldInt;
+        }
+        else if (heldInt == board[1,1] && heldInt == board[2, 2])
+        {
+            winner = heldInt;
+        }
+    }
+
+    if (winner == 0 && (board[1, 0] == 1 || board[1, 0] == 2))
+    {
+        heldInt = board[1, 0];
+        if (heldInt == board[1, 1] && heldInt == board[1, 2])
+        {
+            winner = heldInt;
+        }
+    }
+
+    if (winner == 0 && (board[0,2] == 1 || board[0,2] == 1))
+    {
+        heldInt = board[0,2];
+        if (heldInt == board[1,1] && heldInt == board[2,0])
+        {
+            winner=heldInt;
+        }
+        else if (heldInt == board[1,2] && heldInt == board[2,2])
+        {
+            winner=-heldInt;
+        }
+    }
+
+    if (winner == 0 && (board[2,1] == 1 || board[2,1] == 2))
+    {
+        heldInt=board[2,1];
+        if (heldInt == board[0,1] && heldInt==board[1,1])
+        {
+            winner = heldInt;
+        }
+        else if (heldInt == board[2,0] && heldInt == board[2,2])
+        {
+            winner=heldInt;
+        }
+    }
+    Console.WriteLine();
+    switch(winner)
+    {
+        case 1:
+            Console.WriteLine("Player 1 wins");
+            break;
+        case 2:
+            Console.WriteLine("Player 2 wins");
+            break;
+        default:
+            Console.WriteLine("Nobody wins (HAHAHAHAHA LOSERS)");
+            break;
     }
 }
 
