4b9b5fee81bed0cafbaf91c565f9f49fda4c2c53
Lucas Ladan <l.ladan419@mybvc.ca>
2024-09-19 16:14:05 -0600

    A lot
    I forgot to do this :((((((

--- Program.cs
+++ Program.cs
@@ -1,17 +1,53 @@
 ﻿// See https://aka.ms/new-console-template for more information
+bool badInput = true;
+
 LearningActivity31();
 LearningActivity32();
 LearningActivity33();
 LearningActivity34();
 LearningActivity35();
 
+int interagerInput(string prompt)//This is to make sure that the entered number is a valid interager
+{
+    int a = 0;
+    do
+    {
+        Console.WriteLine(prompt);
+    }while (!(int.TryParse(Console.ReadLine(), out a)));
+    return a;
+}
 void LearningActivity31()
 {
-    Console.WriteLine("This is the function for Learning Activity 3.1!");
+    float temperature = 0F;
+    do
+    {
+        Console.WriteLine("Please enter the temperature");
+    }while (!(float.TryParse(Console.ReadLine(),out temperature)));
+    int whatUnit = 0;
+    do
+    {
+        whatUnit = interagerInput("What unit are you using?\n1 for celsius\n2 for fahrenheit");
+    } while (whatUnit != 1 && whatUnit != 2);
+    if (whatUnit == 1)
+    {
+        temperature = (temperature * (9F / 5F)) + 32F;
+        Console.WriteLine("The temperature in fahrenheit is " + temperature);
+    }
+    else
+    {
+        temperature = (temperature - 32F) * (5F / 9F);
+        Console.WriteLine("The temperature in celsius is " + temperature);
+    }
 }
 void LearningActivity32()
 {
-    Console.WriteLine("This is the function for Learning Activity 3.2!");
+    string[] names = new string[14];
+    int[] ages = new int[14];
+    string[] wonders = {"Great Wall of China","Petra","Colosseum","Chichen Itza","Machi Picchu","Taj Mahal","Christ the Redeemer"};
+    for (int i = 0; i < wonders.Length; i++)
+    {
+        Console.WriteLine("the " + i + 1 + " wonder of the world is" + wonders[i]);
+    }
 }
 void LearningActivity33()
 {
@@ -28,6 +64,12 @@ void LearningActivity34()
 void LearningActivity35()
 {
     int[] highScores = { 1272700, 1271100, 1243000, 1218000, 1214300, 1210800, 1210400, 1206800, 1178400 };
-    Console.WriteLine("This is the function for Learning Activity 3.5!");
+    float average = 0F;
+    for (int i = 0; i < highScores.Length; i++)
+    {
+        average += highScores[i];
+    }
+    average /= highScores.Length;
+    Console.WriteLine("The average is: "+average);
 }
 
